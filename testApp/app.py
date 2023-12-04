c4acb25fb98ea71f4e6dbf190ebc15527e3f99db
Thubelihle Dhliwayo (Thuba) <tmd17@student.le.ac.uk>
2023-12-04 02:49:02 +0000

    Update app.py to include the messages array, and image generation
    messages array is a global array that stores all the user prompts and the the bot response. the initial message is used to describe the role of the chat bot, and "messages.append" adds the user prompt/bot response into the array list for the chatbot to make reference to.

--- testApp/app.py
+++ testApp/app.py
@@ -1,45 +1,63 @@
 """ Flask app to run a server """
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, Response
 import openai
-from openai import OpenAI
-client = OpenAI()
+#from openai import OpenAI
+#client = OpenAI(
+#    api_key="API_KEY"
+#)
 
 app = Flask(__name__)
 
 # Set your OpenAI API key here
-openai.api_key = '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'
+openai.api_key = 'API_KEY'
 
 @app.route('/')
+
 def home():
     """Render the home page"""
     return render_template('index.html')
+
+messages = [
+    {"role": "system", "content": "You are a helpful assistant."},
+]
 
 @app.route('/generate_response', methods=['POST'])
 def generate_response():
     """generate response from gpt-3.5-turbo"""
     user_input = request.form['user_input']
 
-    # Call the OpenAI API to get a response
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: "+user_input+"\nAI:"},
-            {"role": "user", "content": user_input,}
-            ],
-        max_tokens=2200,
-    )
+    # Append the user's message to the conversation histroy(messages array)
+    messages.append({"role": "user", "content": user_input})
 
-    print(response.choices[0].message)
-    
-    if not isinstance(response, dict):
-        print(f"Unexpected response type: {type(response)}")
+    # Call the OpenAI API to get a response
+    response = openai.chat.completions.create(
+    messages=messages,
+    model="gpt-3.5-turbo",
+)
+    ai_response = response.choices[0].message.content
+    print(ai_response)
+    if not isinstance(ai_response, str):
+        print(f"Unexpected response type: {type(ai_response)}")
         return jsonify({'bot_response': 'An error occurred'})
 
-    # bot_response = response.choices[0].message.content
+    bot_response = ai_response
 
-    # You can store the conversation history in a database or in-memory data structure
+    # Append the bot_response to the conversation history(messages array)
+    messages.append({"role": "assistant", "content": bot_response})
 
-    return jsonify({'bot_response': response})
+    # generate an image with dall-e using the users prompt
+    image_response = openai.images.generate(
+        model = "dall-e-3",
+        prompt = user_input + bot_response,
+        size = "1024x1024",
+        quality = "standard",
+        n=1,
+    )
+
+    print (image_response.__dict__)
+    image_url = image_response.data[0].url
+
+    return jsonify({'bot_response': bot_response, 'image_url': image_url})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
