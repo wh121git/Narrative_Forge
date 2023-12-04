6e8f6e8ee29db8e1d27b31cf0fbfc7f1b453a441
thvba <tmd17@student.le.ac.uk>
2023-12-04 15:16:53 +0000

    Made things better and then worse

---
+++ UI/app.py
@@ -0,0 +1,63 @@
+""" Flask app to run a server """
+from flask import Flask, render_template, request, jsonify, Response
+import openai
+#from openai import OpenAI
+#client = OpenAI(
+#    api_key="API_KEY"
+#)
+
+app = Flask(__name__)
+
+# Set your OpenAI API key here
+openai.api_key = 'API_KEY'
+
+@app.route('/')
+
+def home():
+    """Render the home page"""
+    return render_template('NarrativeForge.html')
+
+messages = [
+    {"role": "system", "content": "You are a helpful assistant."},
+]
+
+@app.route('/generate_response', methods=['POST'])
+def generate_response():
+    """generate response from gpt-3.5-turbo"""
+    user_input = request.form['myInput']
+
+    # Append the user's message to the conversation histroy(messages array)
+    messages.append({"role": "user", "content": user_input})
+
+    # Call the OpenAI API to get a response
+    response = openai.chat.completions.create(
+    messages=messages,
+    model="gpt-3.5-turbo",
+)
+    ai_response = response.choices[0].message.content
+    print(ai_response)
+    if not isinstance(ai_response, str):
+        print(f"Unexpected response type: {type(ai_response)}")
+        return jsonify({'bot_response': 'An error occurred'})
+
+    bot_response = ai_response
+
+    # Append the bot_response to the conversation history(messages array)
+    messages.append({"role": "assistant", "content": bot_response})
+
+    # generate an image with dall-e using the users prompt
+    image_response = openai.images.generate(
+        model = "dall-e-3",
+        prompt = user_input + bot_response,
+        size = "1024x1024",
+        quality = "standard",
+        n=1,
+    )
+
+    print (image_response.__dict__)
+    image_url = image_response.data[0].url
+
+    return jsonify({'bot_response': bot_response, 'image_url': image_url})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
---
+++ UI/static/scripts.js
@@ -0,0 +1,174 @@
+/*!
+* Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+var selectedOption; // Variable to store the selected option
+var storyScale = ['Introduction', 'Rising Action', 'Climax', 'Falling Action', 'Conclusion'];
+var scalePosition = 0;
+
+// Function to update the story scale display
+function updateScale() {
+    var scalePositionElement = document.getElementById("scalePosition");
+    scalePositionElement.innerText = storyScale[scalePosition];
+}
+// retrieves the input from the usera and displays the input on the page
+function handleInput() {
+    var userInput = document.getElementById("myInput").value;
+    var displayArea = document.getElementById("displayArea");
+    displayArea.innerHTML = "<p>You entered: " + userInput + "</p>";
+    // remember this clears the text area
+    document.getElementById("myInput").value = "";
+
+    // Show the option buttons after displaying the text
+    document.getElementById("optionButtons").style.display = "block";
+
+    // Show the image container after displaying the buttons
+    document.getElementById("imageContainer").style.display = "flex";
+
+}
+
+// Sidebar stuff
+function w3_open() {
+    var sidebar = document.getElementById("mySidebar");
+    sidebar.style.display = "block";
+    sidebar.classList.add("teal-background");
+}
+
+function w3_close() {
+    var sidebar = document.getElementById("mySidebar");
+    sidebar.style.display = "none";
+    sidebar.classList.remove("teal-background");
+}
+
+// get the selected option
+function selectOption(option) {
+    selectedOption = option;
+    // alert("Selected Option: " + selectedOption);
+
+     // Move to the next stop in the story scale
+     if (scalePosition < storyScale.length - 1) {
+        scalePosition++;
+        updateScale();
+    }
+
+    // Check if it's the last stop, then disable the buttons
+    if (scalePosition === storyScale.length - 1) {
+        disableOptionButtons();
+    }
+}
+
+        // Function to disable the option buttons
+    function disableOptionButtons() {
+    document.getElementById("optionButtons").style.pointerEvents = "none";
+    alert("You have reached the end of the story, you can no longer change the narrative.");
+}
+
+// js/script.js
+function sendMessage() {
+    let userInput = document.getElementById('myInput').value;
+
+    // Display user message
+    appendMessage('You', userInput);
+
+    // Send user message to Flask backend
+    fetch('/generate_response', {
+        method: 'POST',
+        body: `myInput=${encodeURIComponent(userInput)}`,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Get the image URL from the response
+    let imageUrl = data.image_url;
+
+    // Create an img element
+    let img = document.createElement('img');
+    img.src = imageUrl;
+
+    // Append the img element to the imageContainer
+    let imageContainer = document.getElementById('imageContainer');
+    imageContainer.appendChild(img);
+
+    // Make the imageContainer visible
+    imageContainer.style.display = 'block';
+})
+.catch(error => console.error('Error:', error));
+
+    // const img = document.createElement('img-response');
+   // img.src = data.image_url;
+    //console.log(data.image_url);
+   // document.body.appendChild(img);
+   // })
+    //.catch(error => console.error('Error:', error));    
+}
+
+
+function appendMessage(sender, message) {
+    let chatContainer = document.getElementById('displayArea');
+    chatContainer.innerHTML += `<p><strong>${sender}:</strong> ${message}</p>`;
+}
+
+function appendImage(imageUrl) {
+    let chatContainer = document.getElementById('imageContainer');
+    let img = document.createElement('img');
+    img.src = imageUrl;
+    img.classList.add('img-response'); // Add the class to the image
+    chatContainer.appendChild(img);
+}
+
+
+
+
