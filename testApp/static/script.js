c34220975cd15da878e378eb5e42ad1088385b34
Thubelihle Dhliwayo (Thuba) <tmd17@student.le.ac.uk>
2023-12-04 02:50:25 +0000

    Update script.js to include the image from app.py

--- testApp/static/script.js
+++ testApp/static/script.js
@@ -17,10 +17,28 @@ function sendMessage() {
     .then(data => {
         // Display bot response
         appendMessage('Bot', data.bot_response);
-    });
+
+        // Display the image
+        appendImage(data.image_url);
+
+    const img = document.createElement('img-response');
+    img.src = data.image_url;
+    console.log(data.image_url);
+    document.body.appendChild(img);
+    })
+    .catch(error => console.error('Error:', error));    
 }
+
 
 function appendMessage(sender, message) {
     let chatContainer = document.getElementById('chat');
     chatContainer.innerHTML += `<p><strong>${sender}:</strong> ${message}</p>`;
+}
+
+function appendImage(imageUrl) {
+    let chatContainer = document.getElementById('chat');
+    let img = document.createElement('img');
+    img.src = imageUrl;
+    img.classList.add('img-response'); // Add the class to the image
+    chatContainer.appendChild(img);
 }
